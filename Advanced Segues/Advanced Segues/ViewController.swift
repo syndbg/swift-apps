1240fd4a6d49fe2188c794a5d37a2cfc50a7f77d
Anton Antonov <anton.synd.antonov@gmail.com>
2015-07-18 14:12:07 +0300

    Added Advanced Segues example app

---
+++ Advanced Segues/Advanced Segues/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Advanced Segues
+//
+//  Created by Anton Antonov on 7/18/15.
+//  Copyright (c) 2015 Anton Antonov. All rights reserved.
+//
+
+import UIKit
+
+var rowCounter:Int = 0
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
